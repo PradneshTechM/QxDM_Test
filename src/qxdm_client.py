54a204d65bd0526e28215c9e085e7f8f81cb0dcf
Jeffrey Yang <JY00614938@techmahindra.com>
2020-10-12 10:37:37 -0700

    Fix linting issues in QXDM server and client

--- src/qxdm_client.py
+++ src/qxdm_client.py
@@ -29,106 +29,109 @@ SAVE_FILE_PATH_4 = CLIENT_TEST_FOLDER / 'saved_test_4.isf'
 
 
 def test_status():
-  logging.info('Status request')
-  response = _worker_stub_singleton.Status(qxdm_pb2.StatusRequest())
-  logging.info(f'Status ready: {response.ready}')
-  return response.ready
+    logging.info('Status request')
+    response = _worker_stub_singleton.Status(qxdm_pb2.StatusRequest())
+    logging.info(f'Status ready: {response.ready}')
+    return response.ready
 
 
 def test_connect(device_index):
-  logging.info('Connect request')
-  response = _worker_stub_singleton.Connect(
-    qxdm_pb2.ConnectRequest(device_index=device_index))
-  return response.state
+    logging.info('Connect request')
+    response = _worker_stub_singleton.Connect(
+        qxdm_pb2.ConnectRequest(device_index=device_index))
+    return response.state
 
 
 def test_disconnect(device_index):
-  logging.info('Disconnect request')
-  response = _worker_stub_singleton.Disconnect(
-    qxdm_pb2.DisconnectRequest(device_index=device_index))
-  return response.state
+    logging.info('Disconnect request')
+    response = _worker_stub_singleton.Disconnect(
+        qxdm_pb2.DisconnectRequest(device_index=device_index))
+    return response.state
 
 
 def test_start_log(device_index):
-  logging.info('Start log request')
-  _worker_stub_singleton.StartLog(
-    qxdm_pb2.StartLogRequest(device_index=device_index))
+    logging.info('Start log request')
+    _worker_stub_singleton.StartLog(
+        qxdm_pb2.StartLogRequest(device_index=device_index))
 
 
 def test_stop_log(device_index, log_path):
-  logging.info('Stop log request')
-  responses = _worker_stub_singleton.SaveLog(
-    qxdm_pb2.SaveLogRequest(device_index=device_index))
+    logging.info('Stop log request')
+    responses = _worker_stub_singleton.SaveLog(
+        qxdm_pb2.SaveLogRequest(device_index=device_index))
 
-  with open(log_path, 'wb') as file_:
-    for response in responses:
-      file_.write(response.data)
-  logging.info(f'Saved log to {log_path}')
+    with open(log_path, 'wb') as file_:
+        for response in responses:
+            file_.write(response.data)
+    logging.info(f'Saved log to {log_path}')
 
 
 def _shutdown_worker():
-  logging.info('Shutting worker process down.')
-  if _worker_channel_singleton is not None:
-    _worker_channel_singleton.stop()
+    logging.info('Shutting worker process down.')
+    if _worker_channel_singleton is not None:
+        _worker_channel_singleton.stop()
 
 
 def _initialize_worker(server_address):
-  global _worker_channel_singleton
-  global _worker_stub_singleton
-  logging.info('Initializing worker process.')
-  _worker_channel_singleton = grpc.insecure_channel(server_address)
-  _worker_stub_singleton = qxdm_pb2_grpc.QXDMStub(_worker_channel_singleton)
-  atexit.register(_shutdown_worker)
+    global _worker_channel_singleton
+    global _worker_stub_singleton
+    logging.info('Initializing worker process.')
+    _worker_channel_singleton = grpc.insecure_channel(server_address)
+    _worker_stub_singleton = qxdm_pb2_grpc.QXDMStub(_worker_channel_singleton)
+    atexit.register(_shutdown_worker)
 
 
 def _run_multiprocess_test(server_address, test, args):
-  worker_pool = multiprocessing.Pool(
-    processes=_PROCESS_COUNT,
-    initializer=_initialize_worker,
-    initargs=(server_address,))
-  worker_pool.starmap(test, args)
+    worker_pool = multiprocessing.Pool(
+        processes=_PROCESS_COUNT,
+        initializer=_initialize_worker,
+        initargs=(server_address,))
+    worker_pool.starmap(test, args)
 
 
 def test_connect_e2e(device_index):
-  test_connect(device_index)
-  sleep(2)
-  test_disconnect(device_index)
+    test_connect(device_index)
+    sleep(2)
+    test_disconnect(device_index)
 
 
 def test_log_e2e(device_index, log_path):
-  if not test_status():
-    return
-  test_connect(device_index)
-  sleep(2)
-  test_start_log(device_index)
-  sleep(2)
-  test_stop_log(device_index, log_path)
-  sleep(2)
-  test_disconnect(device_index)
-  
+    if not test_status():
+        return
+    test_connect(device_index)
+    sleep(2)
+    test_start_log(device_index)
+    sleep(2)
+    test_stop_log(device_index, log_path)
+    sleep(2)
+    test_disconnect(device_index)
+
 
 def main():
-  server_address = 'localhost:40041'
+    server_address = 'localhost:40041'
 
-  # remove files in client_test folder
-  if CLIENT_TEST_FOLDER.exists() and CLIENT_TEST_FOLDER.is_dir():
-    shutil.rmtree(CLIENT_TEST_FOLDER)
-  os.mkdir(CLIENT_TEST_FOLDER)
-  
-  # _run_multiprocess_test(server_address, test_connect_e2e, [[0], [1], [2], [3], [4]])
+    # remove files in client_test folder
+    if CLIENT_TEST_FOLDER.exists() and CLIENT_TEST_FOLDER.is_dir():
+        shutil.rmtree(CLIENT_TEST_FOLDER)
+    os.mkdir(CLIENT_TEST_FOLDER)
 
-  _run_multiprocess_test(server_address, test_status, [[]])
-  
-  _run_multiprocess_test(server_address, test_log_e2e, [
-    [0, SAVE_FILE_PATH_1],
-    [1, SAVE_FILE_PATH_2],
-    [2, SAVE_FILE_PATH_3],
-    [3, SAVE_FILE_PATH_4]
-  ])
+    # _run_multiprocess_test(server_address, test_connect_e2e,
+    #                        [[0], [1], [2], [3], [4]])
+
+    _run_multiprocess_test(server_address, test_status, [[]])
+
+    _run_multiprocess_test(server_address, test_log_e2e, [
+        [0, SAVE_FILE_PATH_1],
+        [1, SAVE_FILE_PATH_2],
+        # [2, SAVE_FILE_PATH_3],
+        # [3, SAVE_FILE_PATH_4]
+    ])
 
 
-if __name__ == '__main__':  
-  handler = logging.StreamHandler(sys.stdout)
-  format = '[PID %(process)d %(asctime)s.%(msecs)03d]: %(message)s'
-  logging.basicConfig(format=format, level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
-  main()
+if __name__ == '__main__':
+    handler = logging.StreamHandler(sys.stdout)
+    format = '[PID %(process)d %(asctime)s.%(msecs)03d]: %(message)s'
+    logging.basicConfig(format=format,
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    main()
--- src/qxdm_server.py
+++ src/qxdm_server.py
@@ -26,12 +26,20 @@ This gRPC server handles the following requests:
 4. stop logging for a device index
 5. QXDM status
 
-qxdm_lib.QXDM manages active sessions.  If QXDM stops running (e.g. crashes, power failure), all connections and logging is lost anyways so no external database is necessary - server will automatically restart QXDM itself or when a request comes in.
+qxdm_lib.QXDM manages active sessions.  If QXDM stops running (e.g.
+crashes, power failure), all connections and logging is lost anyways so no
+external database is necessary - server will automatically restart QXDM itself
+or when a request comes in.
 
 Known issues:
-* if QXDM isn't running between qxdm.process_running() calls, yet the library function is called, an unhandled exception is thrown - server will recover after _PROCESS_CHECK_INTERVAL seconds if QXDM can be launched successfully.
-* if QXDM can't be run the first time, e.g. due to no/invalid license, the following exception is raised:
-  gi.repository.GLib.Error: g-dbus-error-quark: GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown: The name com.qcom.QXDM was not provided by any .service files (2)
+* if QXDM isn't running between qxdm.process_running() calls, yet the library
+function is called, an unhandled exception is thrown - server will recover
+after _PROCESS_CHECK_INTERVAL seconds if QXDM can be launched successfully.
+* if QXDM can't be run the first time, e.g. due to no/invalid license, the
+following exception is raised:
+    gi.repository.GLib.Error: g-dbus-error-quark:
+    GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown: The name
+    com.qcom.QXDM was not provided by any .service files (2)
 """
 
 qxdm = None
@@ -45,175 +53,164 @@ _CHUNK_SIZE = 1024
 
 
 def read_bytes(file_, num_bytes):
-  while True:
-    buf = file_.read(num_bytes)
-    if not buf:
-      break
-    yield buf
+    while True:
+        buf = file_.read(num_bytes)
+        if not buf:
+            break
+        yield buf
 
 
 # https://stackoverflow.com/a/28034554
-def do_every(period,f,*args):
-  def g_tick():
-    t = time.time()
-    count = 0
+def do_every(period, f, *args):
+    def g_tick():
+        t = time.time()
+        count = 0
+        while True:
+            count += 1
+            yield max(t + count*period - time.time(), 0)
+    g = g_tick()
     while True:
-        count += 1
-        yield max(t + count*period - time.time(),0)
-  g = g_tick()
-  while True:
-    time.sleep(next(g))
-    f(*args)
+        time.sleep(next(g))
+        f(*args)
 
 
 class HiddenPrints:
-  def __enter__(self):
-    self._original_stdout = sys.stdout
-    sys.stdout = open(os.devnull, 'w')
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
 
-  def __exit__(self, exc_type, exc_val, exc_tb):
-    sys.stdout.close()
-    sys.stdout = self._original_stdout
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
 
 
 class QXDMServicer(qxdm_pb2_grpc.QXDMServicer):
+    def Connect(self, request, context):
+        with qxdm_lock:
+            logging.info(f'[Connect] request on device_index: {request.device_index}')
 
-  def Connect(self, request, context):
-    with qxdm_lock:
-      logging.info(f'[Connect] request\t\t| device_index: {request.device_index}')
+            if not qxdm.process_running():
+                context.abort(code=grpc.StatusCode.FAILED_PRECONDITION,
+                              details='QXDM not running. Try again later.')
+                launch_qxdm_if_not_running()
 
-      if not qxdm.process_running():
-        context.abort(
-          code=grpc.StatusCode.FAILED_PRECONDITION,
-          details='QXDM not running. Try again later.'
-        )
-        launch_qxdm_if_not_running()
+            connected = qxdm.connect(request.device_index)
 
-      connected = qxdm.connect(request.device_index)
+            if connected:
+                logging.info(f'[Connect] Connected: {request.device_index}')
+                connection_state = qxdm_pb2.CONNECTED
+            else:
+                logging.info(f'[Connect] Could not connect: {request.device_index}')
+                connection_state = qxdm_pb2.DISCONNECTED
 
-      if connected:
-        logging.info(f'[Connect] Connected: {request.device_index}')
-        connection_state = qxdm_pb2.CONNECTED
-      else:
-        logging.info(f'[Connect] Could not connect: {request.device_index}')
-        connection_state = qxdm_pb2.DISCONNECTED
+            return qxdm_pb2.ConnectResponse(state=connection_state)
 
-      return qxdm_pb2.ConnectResponse(state=connection_state)
+    def Disconnect(self, request, context):
+        logging.info(f'[Disconnect] request on device_index: {request.device_index}')
 
+        if not qxdm.process_running():
+            context.abort(code=grpc.StatusCode.FAILED_PRECONDITION,
+                          details='QXDM not running. Try again later.')
+            launch_qxdm_if_not_running()
 
-  def Disconnect(self, request, context):
-    logging.info(f'[Disconnect] request\t| device_index: {request.device_index}')
+        connected = qxdm.disconnect(request.device_index)
 
-    if not qxdm.process_running():
-      context.abort(
-        code=grpc.StatusCode.FAILED_PRECONDITION,
-        details='QXDM not running. Try again later.'
-      )
-      launch_qxdm_if_not_running()
-
-    connected = qxdm.disconnect(request.device_index)
-
-    if connected:
-      logging.info(f'[Disconnect] Disconnected: {request.device_index}')
-      connection_state = qxdm_pb2.CONNECTED
-    else:
-      logging.info(f'[Disconnect] Could not disconnect: {request.device_index}')
-      connection_state = qxdm_pb2.DISCONNECTED
-
-    return qxdm_pb2.DisconnectResponse(state=connection_state)
-
-
-  def StartLog(self, request, context):
-    logging.info(f'[StartLog] request\t\t| device_index: {request.device_index}')
-
-    if not qxdm.process_running():
-      context.abort(
-        code=grpc.StatusCode.FAILED_PRECONDITION,
-        details='QXDM not running. Try again later.'
-      )
-      launch_qxdm_if_not_running()
-
-    qxdm.start_logs(request.device_index)
-
-    return qxdm_pb2.StartLogResponse()
-
-
-  def SaveLog(self, request, context):
-    logging.info(f'[SaveLog] request\t\t| device_index: {request.device_index}')
-
-    if not qxdm.process_running():
-      context.abort(
-        code=grpc.StatusCode.FAILED_PRECONDITION,
-        details='QXDM not running. Try again later.'
-      )
-      launch_qxdm_if_not_running()
-
-    filepath = qxdm.save_logs(request.device_index)
-
-    # open file and send data in chunks
-    with open(filepath, 'rb') as file_content:
-      for rec in read_bytes(file_content, _CHUNK_SIZE):
-        yield qxdm_pb2.SaveLogResponse(data=rec)
-    
-    logging.info(f'[SaveLog] Sent logs: {request.device_index}')
-
-
-  def Status(self, request, context):
-    ready = qxdm.process_running()
-    if not ready:
-      # launch in another thread
-      threading.Thread(target=launch_qxdm_if_not_running)
-
-    logging.info(f'[Status] request, response: {ready}')
-
-    return qxdm_pb2.StatusResponse(ready=ready)
-
-
-def launch_qxdm_if_not_running(log = 'QXDM re-launched.'):
-  global qxdm, launching_lock
-
-  # only 1 thread with launching_lock can execute below
-  with launching_lock:
-    # qxdm hasn't been started before, or it was running but not anymore
-    if not qxdm or (qxdm and not qxdm.process_running()):
-      try:
-        qxdm = qxdm_lib.QXDM()
-        if qxdm.process_running():
-          logging.info(log)
+        if connected:
+            logging.info(f'[Disconnect] Disconnected: {request.device_index}')
+            connection_state = qxdm_pb2.CONNECTED
         else:
-          logging.info('QXDM could not be re-launched.')
-      except GLib.Error:
-        logging.info('Could not connect to QXDM Dbus. Check license.')
+            logging.info(f'[Disconnect] Could not disconnect: {request.device_index}')
+            connection_state = qxdm_pb2.DISCONNECTED
+
+        return qxdm_pb2.DisconnectResponse(state=connection_state)
+
+    def StartLog(self, request, context):
+        logging.info(f'[StartLog] request on device_index: {request.device_index}')
+
+        if not qxdm.process_running():
+            context.abort(code=grpc.StatusCode.FAILED_PRECONDITION,
+                          details='QXDM not running. Try again later.')
+            launch_qxdm_if_not_running()
+
+        qxdm.start_logs(request.device_index)
+
+        return qxdm_pb2.StartLogResponse()
+
+    def SaveLog(self, request, context):
+        logging.info(f'[SaveLog] request on device_index: {request.device_index}')
+
+        if not qxdm.process_running():
+            context.abort(code=grpc.StatusCode.FAILED_PRECONDITION,
+                          details='QXDM not running. Try again later.')
+            launch_qxdm_if_not_running()
+
+        filepath = qxdm.save_logs(request.device_index)
+
+        # open file and send data in chunks
+        with open(filepath, 'rb') as file_content:
+            for rec in read_bytes(file_content, _CHUNK_SIZE):
+                yield qxdm_pb2.SaveLogResponse(data=rec)
+
+        logging.info(f'[SaveLog] Sent logs: {request.device_index}')
+
+    def Status(self, request, context):
+        ready = qxdm.process_running()
+        if not ready:
+            # launch in another thread
+            threading.Thread(target=launch_qxdm_if_not_running)
+
+        logging.info(f'[Status] request, response: {ready}')
+
+        return qxdm_pb2.StatusResponse(ready=ready)
+
+
+def launch_qxdm_if_not_running(log='QXDM re-launched.'):
+    global qxdm, launching_lock
+
+    # only 1 thread with launching_lock can execute below
+    with launching_lock:
+        # qxdm hasn't been started before, or it was running but not anymore
+        if not qxdm or (qxdm and not qxdm.process_running()):
+            try:
+                qxdm = qxdm_lib.QXDM()
+                if qxdm.process_running():
+                    logging.info(log)
+                else:
+                    logging.info('QXDM could not be re-launched.')
+            except GLib.Error:
+                logging.info('Could not connect to QXDM Dbus. Check license.')
 
 
 def main():
-  bind_address = 'localhost:40041'
+    bind_address = 'localhost:40041'
 
-  with Xvfb(width=2, height=2, colordepth=8):
-    with HiddenPrints():
-      launch_qxdm_if_not_running('QXDM launched.')
+    with Xvfb(width=2, height=2, colordepth=8):
+        with HiddenPrints():
+            launch_qxdm_if_not_running('QXDM launched.')
 
-      server = grpc.server(futures.ThreadPoolExecutor(
-        max_workers=10, 
-        thread_name_prefix='Thread'))
-      qxdm_pb2_grpc.add_QXDMServicer_to_server(QXDMServicer(), server)
-      server.add_insecure_port(bind_address)
-      logging.info(f'Listening on {bind_address}')
-      server.start()
+            server = grpc.server(futures.ThreadPoolExecutor(
+                max_workers=10,
+                thread_name_prefix='Thread'))
+            qxdm_pb2_grpc.add_QXDMServicer_to_server(QXDMServicer(), server)
+            server.add_insecure_port(bind_address)
+            logging.info(f'Listening on {bind_address}')
+            server.start()
 
-      # remove files in temp folder
-      if _TEMP_FOLDER_PATH.exists() and _TEMP_FOLDER_PATH.is_dir():
-        shutil.rmtree(_TEMP_FOLDER_PATH)
-      os.mkdir(_TEMP_FOLDER_PATH)
+            # remove files in temp folder
+            if _TEMP_FOLDER_PATH.exists() and _TEMP_FOLDER_PATH.is_dir():
+                shutil.rmtree(_TEMP_FOLDER_PATH)
+            os.mkdir(_TEMP_FOLDER_PATH)
 
-      # launch QXDM if it's not running every _PROCESS_CHECK_INTERVAL
-      do_every(_PROCESS_CHECK_INTERVAL, launch_qxdm_if_not_running, 
-        'QXDM not running. Launching it.')
+            # launch QXDM if it's not running every _PROCESS_CHECK_INTERVAL
+            do_every(_PROCESS_CHECK_INTERVAL, launch_qxdm_if_not_running,
+                     'QXDM not running. Launching it.')
 
-      server.wait_for_termination()
+            server.wait_for_termination()
 
 
 if __name__ == '__main__':
-  format_str = '[%(threadName)s %(asctime)s.%(msecs)03d]: %(message)s'
-  logging.basicConfig(format=format_str, level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
-  main()
+    format_str = '[%(threadName)s %(asctime)s.%(msecs)03d]: %(message)s'
+    logging.basicConfig(format=format_str,
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    main()
