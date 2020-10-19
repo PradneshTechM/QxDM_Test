e184a2c681d8b3191ea07d9c77a03908e11fa83e
Jeff Yang <JY00614938@techmahindra.com>
2020-10-19 00:21:05 -0700

    Update TC1 and TC2 automation scripts

--- src/qcat_tests/tc1_lte_latch.py
+++ src/qcat_tests/tc1_lte_latch.py
@@ -7,49 +7,69 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import subprocess
 from subprocess import Popen, PIPE
+import logging
+import requests.packages.urllib3
+import requests
+
+
 
 #from driverInitialization import Driver
 
-dut1 = '96071FFBA00048'
-dialpad_fab_ID = 'com.google.android.dialer:id/dialpad_fab'
+dut1 = '96031FFBA001J1'
 
 
 def init_driver():
     print("*****************Test1 - 4G Attach Procedure - Started *******************************")
+    print("Test Location: Redmond")
+    print("HeadSpin Hostname: proxy-us-pnw-1.headspin.io")
+    print("Test Device Model: Google - Pixel4 XL")
+    print("Creating a driver for Settings Apps on Pixel 4 Device - 96031FFBA001J1")
     desired_caps = {
         "deviceName": "Pixel 4 XL",
-        "udid": "96071FFBA00048",
+        "udid": "96031FFBA001J1",
         "automationName": "UiAutomator2",
         "appPackage": "com.android.settings",
         "platformName": "Android",
-        "appPackage": "com.google.android.dialer",
-        "appActivity": ".extensions.GoogleDialtactsActivity",
+        #"appPackage": "com.google.android.dialer",
+        #"appActivity": ".extensions.GoogleDialtactsActivity",
+		"appPackage": "com.android.settings",
+        "appActivity": ".homepage.SettingsHomepageActivity",
 		"newCommandTimeout": 3000
     }
     driver = webdriver.Remote('https://us-pnw.headspin.io:7012/v0/4aea75eb1ac3401eb1c95c4362ff68dd/wd/hub',
                               desired_caps)
+    print("Driver is created for Settings App")	
+	
+    #print("Driver details are as follows:")	
+    #print(driver)
+
+    network_element= WebDriverWait(driver, 30).until(
+		EC.element_to_be_clickable((MobileBy.XPATH, "//*[@text=\"Network & internet\"]" ))
+	)
+    network_element.click()
+	
     return driver
 
 
 def enabled_root():
     subprocess.call(
-        "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"root\"",
+        "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"root\"",
         shell=True)
     time.sleep(15)
 
 
 def enable_airplane_mode():
     
-    print("Tuning on Airplane mode..........")
+    print("Turning on Airplane mode..........")
     subprocess.call(
-        "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"settings put global airplane_mode_on 1\"",
+        "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"settings put global airplane_mode_on 1\"",
         shell=True)
 
-    cmd = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"am broadcast -a android.intent.action.AIRPLANE_MODE\""
+    cmd = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"am broadcast -a android.intent.action.AIRPLANE_MODE\""
     status, output = subprocess.getstatusoutput(cmd)
-    print("+++++++++++************+++++" + output + "+++++++++++++**************+++")
+    #print("+++++++++++************+++++" + output + "+++++++++++++**************+++")
     if (output.__contains__("Broadcast completed")):
-        print("Airplane mode on")
+        print("\nAirplane mode on")
 
     else:
         print("Error: Unable to change airplane mode to on")
@@ -58,24 +78,25 @@ def enable_airplane_mode():
 
 
 def disable_airplane_mode():
-    print("Tuning off Airplane mode..........")
+    print("Turning off Airplane mode..........")
 
     subprocess.call(
-        "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"settings put global airplane_mode_on 0\"",
+        "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"settings put global airplane_mode_on 0\"",
         shell=True)
 
-    cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"am broadcast -a android.intent.action.AIRPLANE_MODE\""
+    cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"am broadcast -a android.intent.action.AIRPLANE_MODE\""
     status, output = subprocess.getstatusoutput(cmd1)
-    print("+++++++++++************+++++" + output + "+++++++++++++**************+++")
+    #print("+++++++++++************+++++" + output + "+++++++++++++**************+++")
     if (output.__contains__("Broadcast completed")):
-        print("Airplane mode on")
+        print("\nAirplane mode off")
 
     else:
         print("Error: Unable to change airplane mode to on")
 
+
 def verifyLTE():
     print("Checking if LTE is latched........")
-    cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"dumpsys telephony.registry\""
+    cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"dumpsys telephony.registry\""
     status, output = subprocess.getstatusoutput(cmd1)
     #print("+++++++++++************+++++" + output + "+++++++++++++**************+++")
     for i in range(10):
@@ -86,17 +107,23 @@ def verifyLTE():
             time.sleep(10)
             print("Checking connection status ..........")
 
+
 def main():
+    requests.packages.urllib3.disable_warnings()
     driver = init_driver()
     enable_airplane_mode()
     disable_airplane_mode()
     verifyLTE()
     driver.quit()
+    print("*****************Test1 - 4G Attach Procedure - Completed *******************************")
+
 
 if __name__ == '__main__':
     #enabled_root()
+    requests.packages.urllib3.disable_warnings()
     driver = init_driver()
     enable_airplane_mode()
     disable_airplane_mode()
     verifyLTE()
     driver.quit()
+    print("*****************Test1 - 4G Attach Procedure - Completed *******************************")
--- src/qcat_tests/tc2_VoLTE_Call.py
+++ src/qcat_tests/tc2_VoLTE_Call.py
@@ -4,11 +4,13 @@ import time
 from appium import webdriver
 
 
-def init_driver():
+def init_driver_MO():
     print("*****************Test2 - VoLTE MO to VoLTE MT -  Started*******************************\n")
+    print("Creating MO Device Dialer App Driver")
+    # "udid": "96071FFBA00048",
     desired_caps = {
         "deviceName": "Pixel 4 XL",
-        "udid": "96071FFBA00048",
+        "udid": "96031FFBA001J1",
         "automationName": "UiAutomator2",
         "appPackage": "com.android.settings",
         "platformName": "Android",
@@ -18,19 +20,38 @@ def init_driver():
     }
     driver = webdriver.Remote('https://us-pnw.headspin.io:7012/v0/4aea75eb1ac3401eb1c95c4362ff68dd/wd/hub',
                               desired_caps)
+    print("Created MO Device Dialer App Driver")
+
     return driver
 
+
+def init_driver_MT():
+    print("Creating MT Device Dialer App Driver")
+    # "udid": "96031FFBA001J1",
+    desired_caps = {
+        "deviceName": "Pixel 4 XL",
+        "udid": "96071FFBA00048",
+        "automationName": "UiAutomator2",
+        "platformName": "Android",
+        "appPackage": "com.google.android.dialer",
+        "appActivity": ".extensions.GoogleDialtactsActivity",
+        "newCommandTimeout": 3000
+    }
+    driver = webdriver.Remote('https://us-pnw.headspin.io:7012/v0/4aea75eb1ac3401eb1c95c4362ff68dd/wd/hub',
+                              desired_caps)
+    print("Created MT Device Dialer App Driver")
+    return driver
 
 def callingFunctionality():
 
 	#Calling to Device B from Device A
     
-    cmd = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"am start -a android.intent.action.CALL -d tel:4696137553\""
+    cmd = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"am start -a android.intent.action.CALL -d tel:4696137552\""
     output = subprocess.getstatusoutput(cmd)
-    print("Calling.................")
+    print("Making a Call to MT Device")
 
 	#Checking if Call is dialing or not on Device A
-    cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"dumpsys telephony.registry\""
+    cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"dumpsys telephony.registry\""
     status, output = subprocess.getstatusoutput(cmd1)
 	
     for i in range(5):
@@ -47,7 +68,7 @@ def callingFunctionality():
     print("Checking if Device B is receiving the call")
     for i in range(10):
         call_flag=0
-        cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"dumpsys telephony.registry\""
+        cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"dumpsys telephony.registry\""
         status, output = subprocess.getstatusoutput(cmd1)
         for j in range(5):
             if output.__contains__("mCallState=1"):
@@ -56,31 +77,31 @@ def callingFunctionality():
 				#Accept the call if it's ringing
                 print("Accepting the call on Device B")
                 subprocess.call(
-                "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"input keyevent 5\"", shell=True)
+                "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"input keyevent 5\"", shell=True)
                 call_flag=1
                 break
             else:
                 time.sleep(1)
-                print("Checking if Device B is receivng the call..........")
+                print("Checking if Device B is receiving the call..........")
         if call_flag == 1:
             break
     for i in range(10):
-        cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"dumpsys telephony.registry\""
+        cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"dumpsys telephony.registry\""
         status, output = subprocess.getstatusoutput(cmd1)
         if output.__contains__("mCallState=2"):
-            print("Call connected..")
+            print("\nCall connected..")
             break
         else:
             time.sleep(5)
             print("Checking call status ..........")
     time.sleep(5)
     
-    print("Waitig for call to complete 60 seconds")
-    cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"dumpsys telephony.registry\""
+    print("Waiting for call to complete 60 seconds")
+    cmd1 = "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"dumpsys telephony.registry\""
     
     for i in range(15):
         status, output = subprocess.getstatusoutput(cmd1)
-        print("Checking call status ..........")
+        print("Checking call status .......... ", end="")
         if output.__contains__("mCallState=2"):
             print("Call is not dropped")
             time.sleep(1)
@@ -91,18 +112,23 @@ def callingFunctionality():
        
     # time.sleep(60)
     subprocess.call(
-        "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96071FFBA00048/shell -d \"input keyevent 6\"",
+        "curl -X POST https://7bfc56fb456d447e9337209c889d211e@api-dev.headspin.io/v0/adb/96031FFBA001J1/shell -d \"input keyevent 6\"",
         shell=True)
 
+
 def main():
-    driver = init_driver()
+    driver_MO = init_driver_MO()
+    driver_MT = init_driver_MT()
     callingFunctionality()
-    driver.quit()
-    print("*****************Test2 - VoLTE MO to VoLTE MT -Completed*******************************")
+    driver_MO.quit()
+    driver_MT.quit()
+    print("\n*****************Test2 - VoLTE MO to VoLTE MT -Completed*******************************")
 
 
 if __name__ == '__main__':
-    driver = init_driver()
+    driver_MO = init_driver_MO()
+    driver_MT = init_driver_MT()
     callingFunctionality()
-    driver.quit()
+    driver_MO.quit()
+    driver_MT.quit()
     print("*****************Test2 - VoLTE MO to VoLTE MT -Completed*******************************")
