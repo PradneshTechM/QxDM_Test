4aa18004652c2b7219d9304f65ae57f70f527d09
Jeffrey Yang <JY00614938@techmahindra.com>
2022-08-23 18:58:15 -0700

    Add sudo to apt install lines

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # run after Python3.7 is setup
 
 # pycairo and PyGObject dependencies
-apt install libcairo2-dev pkg-config python3-dev gcc libgirepository1.0-dev
-apt install xvfb
+sudo apt install libcairo2-dev pkg-config python3-dev gcc libgirepository1.0-dev
+sudo apt install xvfb
 
 # isntall virtualenv if it's not already
 pip3 install virtualenv --user
