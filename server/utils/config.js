bae013d629f367fb19eea64ff7acf7ece6e0cb81
RamiYahyaTechM <ry0c114076@techmahindra.com>
2023-06-26 01:22:47 -0700

    Update: pass node server side env to python side

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const process = require('process')
+const path = require('path')
+const fs = require('fs')
 
 const NODE_ENV = process.env.NODE_ENV
 const PORT = process.env.PORT !== undefined
@@ -17,6 +19,30 @@ const PROTOCOL = NODE_ENV === 'development'
 const KEY_PATH = process.env.KEY_PATH
 const CERT_PATH = process.env.CERT_PATH
 
+VARIABLES = [
+  "KEY_PATH",
+  "CERT_PATH",
+  "STORAGE_PATH",
+  "DB_NAME",
+  "DB_HOST",
+  "DB_USER",
+  "DB_PASS",
+  "DB_PORT",
+  "DB_TABLE",
+]
+
 module.exports = {
   NODE_ENV, PORT, ADDRESS, DOMAIN, PROTOCOL, KEY_PATH, CERT_PATH
 }
+
+  ; (function createPythonEnv() {
+    const src_path = path.join(__dirname, '..', '..', 'src')
+    const env_path = path.join(src_path, '.env')
+    let content = ""
+    VARIABLES.forEach(key => {
+      if (process.env[key]) {
+        content += `${key}=${process.env[key]}\n`
+      }
+    })
+    fs.writeFileSync(env_path, content)
+  })();
